5486bcfbe215b37efdcff5e45990fc5aa55d6da7
jeanine <j.deregter@student.fontys.nl>
2025-11-03 13:18:34 +0100

    Made content for on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,33 @@
 import IconsColumn from "@/components/ProgramLanguageIcon";
 
-const CodeIcons = [
-  { src: "/nextjs-icon.svg", alt: "Next.js", rotate: 7.32 },
-  { src: "/html5-icon.svg", alt: "HTML5" },
-  { src: "/javascript-icon.svg", alt: "JavaScript" },
-  { src: "/vuejs-icon.svg", alt: "Vue.js" },
-  { src: "/css3-icon.svg", alt: "Vue.js" },
-  { src: "/swiftui-icon.svg", alt: "SwiftUI" },
-];
-
 export default function Home() {
+  const codeIcons = [
+    { src: "/nextjs-icon.svg", alt: "Next.js", rotate: -15.22 },
+    {
+      src: "/html5-icon.svg",
+      alt: "HTML5",
+      rotate: 15.22,
+      className: "ml-35",
+    },
+    { src: "/javascript-icon.svg", alt: "JavaScript", rotate: -15.22 },
+    {
+      src: "/vuejs-icon.svg",
+      alt: "Vue.js",
+      rotate: 15.22,
+      className: "ml-35",
+    },
+    { src: "/css3-icon.svg", alt: "css3.js", rotate: -15.22 },
+    {
+      src: "/swiftui-icon.svg",
+      alt: "SwiftUI",
+      rotate: 15.22,
+      className: "ml-35",
+    },
+  ];
+
   return (
     <main>
-      <section className="homePage flex flex-row ml-[152] mt-[60] items-center">
+      <section className="homePage flex flex-row ml-[152] mr-[92] my-[32] items-center">
         <div className="text-frontpage  w-84">
           <div className="text-title">
             <h2 className="text-text-two text-fontsize-h2 font-h2">
@@ -32,17 +47,19 @@ export default function Home() {
             medische wereld.
           </p>
         </div>
+        <div className="flex-row flex justify-between items-center">
+          <div className="w-1/4 ml-[44%] ">
+            <img
+              className="rounded-lg w-full h-auto relative z-10"
+              src="./picture_jeanine.jpeg"
+              alt="picture"
+            />
+            <div className="absolute top-60 left-232 z-0 h-[340] w-[236] border border-text-three rounded-lg bg-transparent"></div>
+          </div>
 
-        <div className="picture w-1/5 ml-[35%]">
-          <img
-            className="rounded-lg w-full h-auto"
-            src="./picture_jeanine.jpeg"
-            alt="picture"
-          />
-        </div>
-
-        <div className="">
-          <IconsColumn programlanguageicons={CodeIcons} size={64} />
+          <div className="">
+            <IconsColumn className="gap-8" programlanguageicons={codeIcons} />
+          </div>
         </div>
       </section>
     </main>
--- components/ProgramLanguageIcon.tsx
+++ components/ProgramLanguageIcon.tsx
@@ -3,34 +3,44 @@ import Image from "next/image";
 type ProgramLanguageIcon = {
   src: string;
   alt: string;
+  rotate?: number;
+  className?: string;
+  style?: React.CSSProperties;
 };
 
 type IconsColumnProps = {
   programlanguageicons: ProgramLanguageIcon[];
   size?: number;
-  rotate?: number;
   className?: string;
 };
 
 export default function IconsColumn({
   programlanguageicons,
   size = 64,
-  rotate = 0,
   className = "",
 }: IconsColumnProps) {
   return (
     <div className={`flex flex-col ${className}`}>
-      {programlanguageicons.map((icon, index) => (
-        <div key={index} className="shrink-0">
-          <Image
-            src={icon.src}
-            alt={icon.alt}
-            width={size}
-            height={size}
-            className="object-contain  rotate-[${rotate}deg]"
-          />
-        </div>
-      ))}
+      {programlanguageicons.map((icon, index) => {
+        const rotation = icon.rotate ?? 0;
+
+        return (
+          <div
+            key={index}
+            className={`shrink-0 ${icon.className ?? ""}`}
+            style={{ ...icon.style }}
+          >
+            <Image
+              src={icon.src}
+              alt={icon.alt}
+              width={size}
+              height={size}
+              style={{ rotate: `${rotation}deg`, ...icon.style }}
+              className="object-contain"
+            />
+          </div>
+        );
+      })}
     </div>
   );
 }
